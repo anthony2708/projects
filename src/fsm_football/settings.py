de01baf9052d0e621d321890dd7ff6654a96c6e8
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-11-14 07:36:56 +0700

    211114 - BLTAnh - Hide secret key

--- src/fsm_football/settings.py
+++ src/fsm_football/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--kr0s@966ognd20mm8gz9j2mnlhznzrbmze(bvoa%uenp-mg^g'
+# SECRET_KEY = 'django-insecure--kr0s@966ognd20mm8gz9j2mnlhznzrbmze(bvoa%uenp-mg^g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
