4d3768aa0a23d36b0855de578f825e2a020f7056
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-03 10:35:11 +0700

    211203 - BLTAnh - Timeline/Login-Signup

--- src/fsm_football/urls.py
+++ src/fsm_football/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('fsm-football/', include('fsm_admin.urls')),
     path('admin/', admin.site.urls),
+    path('fsm-football/', include('fsm_admin.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
