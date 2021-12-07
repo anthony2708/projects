f8ad882a183c2d2578b80009f4eaca8a5157981c
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-07 19:20:38 +0700

    211207 - BLTAnh - LOGIN

--- src/fsm_admin/views.py
+++ src/fsm_admin/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 
 def index(req):
-    return HttpResponse("Hello world, this is FSM-Football")
+    return render(req, 'home.html')
+
+
+def register(req):
+    if req.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if req.method == 'POST':
+            form = CreateUserForm(req.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+
+    context = {'form': form}
+    return render(req, 'accounts/register.html', context)
+
+
+def login(req):
+    if req.method == 'POST':
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None:
+            login(req, user)
+            redirect('')
+
+        else:
+            messages.error(req, 'Username or Password is incorrect')
+
+    context = {}
+    return render(req, 'accounts/login.html', context)
+
+
+def logoutUser(req):
+    logout(req)
+    return redirect('login')
