fae490f17ccf6de2c058aaedfeacbee2438bd517
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-12 08:33:06 +0700

    211212 - BLTAnh - Some changes

--- CN01_SDN/src/topo1.py
+++ CN01_SDN/src/topo1.py
@@ -10,20 +10,21 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
 
+
 def myNetwork():
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+    net = Mininet(topo=None,
+                  build=False,
+                  ipBase='10.0.0.0/8')
 
-    info( '*** Adding controller\n' )
-    c0=net.addController(name='c0',
-                      controller=RemoteController,
-                      ip='127.0.0.1',
-                      protocol='tcp',
-                      port=6633)
+    info('*** Adding controller\n')
+    c0 = net.addController(name='c0',
+                           controller=RemoteController,
+                           ip='127.0.0.1',
+                           protocol='tcp',
+                           port=6633)
 
-    info( '*** Add switches\n')
+    info('*** Add switches\n')
     s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
     s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
@@ -32,13 +33,13 @@ def myNetwork():
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
 
-    info( '*** Add hosts\n')
+    info('*** Add hosts\n')
     h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
     h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
     h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
     h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
 
-    info( '*** Add links\n')
+    info('*** Add links\n')
     net.addLink(s4, h1)
     net.addLink(s5, h2)
     net.addLink(s6, h3)
@@ -52,13 +53,13 @@ def myNetwork():
     net.addLink(s2, s3)
     net.addLink(s5, s6)
 
-    info( '*** Starting network\n')
+    info('*** Starting network\n')
     net.build()
-    info( '*** Starting controllers\n')
+    info('*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
-    info( '*** Starting switches\n')
+    info('*** Starting switches\n')
     net.get('s5').start([c0])
     net.get('s6').start([c0])
     net.get('s3').start([c0])
@@ -67,12 +68,12 @@ def myNetwork():
     net.get('s2').start([c0])
     net.get('s4').start([c0])
 
-    info( '*** Post configure switches and hosts\n')
+    info('*** Post configure switches and hosts\n')
 
     CLI(net)
     net.stop()
 
-if __name__ == '__main__':
-    setLogLevel( 'info' )
-    myNetwork()
 
+if __name__ == '__main__':
+    setLogLevel('info')
+    myNetwork()
