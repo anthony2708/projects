6e2d8c260fca04099da69538620e1d05ccac9a60
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-13 20:51:45 +0700

    211213 - BLTAnh - LoginRequired

--- src/fsm_admin/urls.py
+++ src/fsm_admin/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
     path('createteam', views.createteam, name='createteam'),
     path('createtournaments', views.createtournaments,
          name='createtournaments'),
--- src/fsm_admin/views.py
+++ src/fsm_admin/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 import random
-from .forms import CreateUserForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from fsm_admin.models import GIAIDAU
 # Create your views here.
 
@@ -14,45 +14,42 @@ def index(req):
 
 def signup(req):
     if req.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if req.method == 'POST':
-            form = CreateUserForm(req.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-
-    context = {'form': form}
-    return render(req, 'registration/signup.html', context)
-
-
-def login(req):
+        return redirect('index')
     if req.method == 'POST':
-        username = req.POST.get('username')
-        password = req.POST.get('password')
+        email = req.POST['email']
+        username = req.POST['username']
+        password = req.POST['password']
 
-        user = authenticate(req, username=username, password=password)
+        user = User.objects.create_user(
+            username=username, password=password, email=email)
+        user.save()
+        return redirect('index')
 
+    return render(req, 'registration/signup.html')
+
+
+def signin(req):
+    if req.method == 'POST':
+        uname = req.POST['username']
+        pword = req.POST['password']
+        user = auth.authenticate(username=uname, password=pword)
+        # print(user)
         if user is not None:
-            login(req, user)
-            redirect('')
+            auth.login(req, user)
+            return redirect('index')
 
         else:
             messages.error(req, 'Username or Password is incorrect')
 
-    context = {}
-    return render(req, 'registration/signin.html', context)
-
-# Create your views here.
+    return render(req, 'registration/signin.html')
 
 
-# def index(req):
-#     tournaments = GIAIDAU.objects.all()
+def signout(req):
+    auth.logout(req)
+    return redirect('index')
 
-#     return render(req, "index.html", {'tournaments': tournaments})
 
-
+@login_required(login_url='/signin')
 def createtournaments(request):
     if request.method == 'POST':
         magiaidau = str(random.randint(1, 10000))
@@ -66,92 +63,6 @@ def createtournaments(request):
         chedo_final = 0
         trangthai = request.POST['trangthai']
 
-        # ins = GIAIDAU(ma_giaidau, tengiaidau, sodoithamdu, thethuc, luatuoi, lephi,
-        # loaisan, chedo, trangthai)
-        # ins.save()
-        if chedo == 'Cong khai':
-            chedo_final = 1
-
-        while GIAIDAU.objects.filter(ma_giaidau=magiaidau) == True:
-            magiaidau = str(random.randint(1, 10000))
-
-        st = GIAIDAU(ma_giaidau=magiaidau, ten_giaidau=tengiaidau, sodoi_thamdu=sodoithamdu,
-                     thethuc=thethuc, luatuoi=luatuoi, lephi=lephi, loaisan=loaisan, chedo=chedo_final, trangthai=trangthai)
-        st.save()
-
-        return redirect('index')
-
-    return render(request, "tournament/createtournament.html")
-
-
-def tournament(request, pk):
-    giaidau = GIAIDAU.objects.get(ma_giaidau=pk)
-
-    return render(request, 'tournament/viewtournament.html', {'tournament': giaidau})
-
-
-def search(request):
-    if request.method == 'GET':
-        if request.GET.get('query'):
-            query = request.GET.get('query')
-            return tournament(request, query)
-
-    return render(request, 'tournament/search.html')
-
-
-def edittournament(request, pk):
-    giaidau = GIAIDAU.objects.get(ma_giaidau=pk)
-
-    if request.method == "POST":
-        ten = request.POST.get('new_ten_giaidau')
-        sodoi_thamdu = request.POST.get('new_sodoi_thamdu')
-        thethuc = request.POST.get('new_thethuc')
-        luatuoi = request.POST.get('new_luatuoi')
-        lephi = request.POST.get('new_lephi')
-        loaisan = request.POST.get('new_loaisan')
-        chedo = request.POST.get('new_chedo')
-        trangthai = request.POST.get('new_trangthai')
-        chedo_final = 0
-
-        if chedo == 'Cong khai':
-            chedo_final = 1
-
-        giaidau.ten_giaidau = ten
-        giaidau.sodoi_thamdu = sodoi_thamdu
-        giaidau.thethuc = thethuc,
-        giaidau.luatuoi = luatuoi
-        giaidau.lephi = lephi
-        giaidau.loaisan = loaisan
-        giaidau.chedo = chedo_final
-        giaidau.trangthai = trangthai
-
-        while GIAIDAU.objects.filter(ma_giaidau=magiaidau) == True:
-            magiaidau = str(random.randint(1, 10000))
-
-        giaidau.save()
-
-        return redirect('index')
-
-    return render(request, 'tournament/EditTournament.html', {'tournament': giaidau})
-
-
-def createtournaments(request):
-    if request.method == 'POST':
-        magiaidau = str(random.randint(1, 10000))
-        tengiaidau = request.POST['ten_giaidau']
-        sodoithamdu = request.POST['sodoi_thamdu']
-        thethuc = request.POST['thethuc']
-        luatuoi = request.POST['luatuoi']
-        lephi = request.POST['lephi']
-        loaisan = request.POST['loaisan']
-        chedo = request.POST['chedo']
-        chedo_final = 0
-        trangthai = request.POST['trangthai']
-
-        # ins = GIAIDAU(ma_giaidau, tengiaidau, sodoithamdu, thethuc, luatuoi,
-        # lephi,
-        # loaisan, chedo, trangthai)
-        # ins.save()
         if chedo == 'Cong khai':
             chedo_final = 1
 
@@ -159,9 +70,9 @@ def createtournaments(request):
             magiaidau = str(random.randint(1, 10000))
 
         st = GIAIDAU(ma_giaidau=magiaidau, ten_giaidau=tengiaidau,
-                     sodoi_thamdu=sodoithamdu, thethuc=thethuc,
-                     luatuoi=luatuoi, lephi=lephi, loaisan=loaisan,
-                     chedo=chedo_final, trangthai=trangthai)
+                     sodoi_thamdu=sodoithamdu,
+                     thethuc=thethuc, luatuoi=luatuoi, lephi=lephi,
+                     loaisan=loaisan, chedo=chedo_final, trangthai=trangthai)
         st.save()
 
         return redirect('index')
@@ -169,6 +80,7 @@ def createtournaments(request):
     return render(request, "tournament/createtournament.html")
 
 
+@login_required(login_url='/signin')
 def tournament(request, pk):
     giaidau = GIAIDAU.objects.get(ma_giaidau=pk)
 
@@ -185,6 +97,7 @@ def search(request):
     return render(request, 'tournament/search.html')
 
 
+@login_required(login_url='/signin')
 def edittournament(request, pk):
     giaidau = GIAIDAU.objects.get(ma_giaidau=pk)
 
@@ -221,8 +134,7 @@ def edittournament(request, pk):
     return render(request, 'tournament/EditTournament.html',
                   {'tournament': giaidau})
 
-def signin(req):
-    return render(req, 'registration/signin.html')
 
+@login_required(login_url='/signin')
 def createteam(request):
     return render(request, 'user/createteam.html')
