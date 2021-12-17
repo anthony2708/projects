43a0eb77ae3cc484d6c676e2f92e5fb103508937
19120141 <nqt15963@gmail.com>
2021-12-17 15:16:46 +0700

    211217 - NQToan - Thêm giao diện admin, tạo giải đấu

--- src/fsm_admin/urls.py
+++ src/fsm_admin/urls.py
@@ -11,12 +11,22 @@ urlpatterns = [
     path('createteam', views.createteam, name='createteam'),
     path('tournaments', views.allTournaments, name='tournaments'),
     path('createtournaments', views.createtournaments,
-         name='createtournaments'),
+        name='createtournaments'),
     path('tournament/<str:pk>', views.tournament,
-         name='tournament'),  # view tournament
+        name='tournament'),  # view tournament
     path('search', views.search, name='search'),
     path('edittournament/<str:pk>', views.edittournament,
-         name='edittournament'),  # edit tournament
+        name='edittournament'),  # edit tournament
     path('jointournament/<str:pk>', views.jointournament,
-         name='jointournament')
+        name='jointournament'),
+
+    # Admin site
+    path('admin_site', views.admin_site, name='admin_site'),
+    path('admin_site/<int:tab>', views.admin_site, name='admin_site'),
+    path('admin_site/signin', views.admin_signin, name='admin_signin'),
+    path('admin_site/signout', views.admin_signout, name='admin_signout'),
+    path('admin_site/1/create_tournament', views.admin_create_tournament, name='admin_create_tournament'),
+    path('admin_site/1/create_tournament/back', views.admin_create_tournament_back, name='admin_create_tournament_back'),
+    
+
 ]
--- src/fsm_admin/views.py
+++ src/fsm_admin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 import random
 
 from .forms import CreateUserForm
@@ -46,6 +47,8 @@ def signin(req):
     context = {
         'fail': False,
     }
+    
+
     if req.user.is_authenticated:
         return redirect('/home')
     if req.method == 'POST':
@@ -55,9 +58,9 @@ def signin(req):
 
         user = authenticate(req, username=username, password=password)
 
-        if user is not None:
+        if user is not None and user.get_username() != "admin":
             auth.login(req, user)
-            print(rememberme)
+
             if rememberme is not False:
                 req.session.set_expiry(2629746)  # 1 tháng
             return redirect('/home')
@@ -263,3 +266,124 @@ def jointournament(request, pk):
 
     return render(request, 'tournament/JoinTournament.html',
                   {'giaidau': giaidau, 'userteams': userteams})
+
+
+
+# ================================ Admin site ===================================================
+
+@login_required(login_url='/admin_site/signin')
+def admin_site(req, tab=None):
+    if tab == None:
+        tab = 1
+    context = {}
+    for i in range(4):
+        if i + 1 == tab:
+            tabActive = "tab" + str(i + 1)
+            context[tabActive] = {
+                "active": "active",
+                "show": "show",
+            }
+        else:
+            tabActive = "tab" + str(i + 1)
+            context[tabActive] = {
+                "active": "",
+                "show": "",
+            }
+
+    if req.user.is_authenticated and req.user.username != "admin":
+        auth.logout(req)
+        return redirect('admin_signin')
+    
+    tournaments = GIAIDAU.objects.all()
+
+    context['tournaments'] = tournaments
+
+    return render(req, 'admin_site/home.html', context)
+
+
+def admin_signin(req):
+    context = {
+        'fail': False,
+    }
+    if req.user.is_authenticated:
+        if req.user.username != "admin":
+            auth.logout(req)
+            return redirect('admin_signin')
+        else:
+            return redirect('/admin_site/1')
+
+    if req.method == 'POST':
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None and user.get_username() == "admin":
+            auth.login(req, user)
+            return redirect('/admin_site/1')
+
+        else:
+            messages.error(req, "Tên đăng nhập hoặc mật khẩu không đúng.")
+            context['fail'] = True
+            
+    return render(req, 'admin_site/signin.html', context)
+
+def admin_signout(req):
+    auth.logout(req)
+    return redirect('admin_signin')
+
+@login_required(login_url='/admin_site/signin')
+def admin_create_tournament(req):
+    context = {}
+    tabActive = 1
+    for i in range(4):
+        if i + 1 == tabActive:
+            tab = "tab" + str(i + 1)
+            context[tab] = {
+                "active": "active",
+                "show": "show",
+            }
+        else:
+            tab = "tab" + str(i + 1)
+            context[tab] = {
+                "active": "",
+                "show": "",
+            }
+
+    if req.method == 'POST':
+
+        tengiaidau = req.POST.get('tournamentName')
+        sodoithamdu = req.POST.get('numTeams')
+        thethuc = req.POST.get('format')
+        if thethuc == 1:
+            thethuc = "Vòng loại 1 lượt"
+        else:
+            thethuc = "Vòng loại 2 lượt"
+        luatuoi = req.POST.get('age')
+        lephi = req.POST.get('fee')
+        loaisan = req.POST.get('type')
+        chedo = req.POST.get('viewMode')
+        if chedo == "public":
+            chedo_final = 1
+        else:
+            chedo_final = 0
+        trangthai = "Đang diễn ra"
+
+
+        giaidau = GIAIDAU(ten_giaidau=tengiaidau,
+                              sodoi_thamdu=sodoithamdu,
+                              thethuc=thethuc, luatuoi=luatuoi, lephi=lephi,
+                              loaisan=loaisan, chedo=chedo_final,
+                              trangthai=trangthai)
+        if giaidau is not None:
+                giaidau.save()
+                return redirect('/admin_site/1')
+        else:
+            context['showMsg'] = True
+            messages.error("Tạo giải đấu không thành công")
+
+    return render(req, 'admin_site/create_tournament.html', context)
+
+@login_required(login_url='/admin_site/signin')
+def admin_create_tournament_back(req):
+    return redirect('/admin_site/1')
