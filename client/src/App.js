c132bf154f0106e61a36a85ff3d7033b826ef3d6
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2023-03-20 10:41:33 +0700

    220320 - BLTAnh - Fix App.js to GH

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Học React
+          Học React nào!
         </a>
       </header>
     </div>
