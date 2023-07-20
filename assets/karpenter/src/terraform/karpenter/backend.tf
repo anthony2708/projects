bf037bd06d4ef1e66d0fc313914704c4b1d9a90c
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2023-07-20 09:16:12 +0700

    230720 - BLTAnh - Karpenter outdated

--- assets/karpenter/src/terraform/karpenter/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "anthony-terraform-bucket"
-    dynamodb_table       = "anthony-terraform-state-lock"
-    key                  = "eks/terraform.tfstate"
-    region               = "us-west-2"
-    workspace_key_prefix = "tfstate:"
-  }
-}
--- assets/karpenter/src/terraform/karpenter/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "==> Applying..."
-terraform apply -var "aws_access_key=$AWS_ACCESS_KEY_ID" -var "aws_secret_key=$AWS_SECRET_ACCESS_KEY" -auto-approve -input=false
-
-echo "DONE!!!"
