6838e712bd3d72825441f62824f84c38e8579a12
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2023-07-28 09:23:20 +0700

    230727 - BLTAnh - Remove all

--- .github/CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Contribution
-
-To contribute to this project, please email [builetuananh2708@gmail.com](mailto:builetuananh2708@gmail.com) for more information.
--- app/includes/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bucket_arn" {
-  value = aws_s3_bucket.terraform_state.arn
-}
-
-output "dynamodb_arn" {
-  value = aws_dynamodb_table.terraform_lock.arn
-}
